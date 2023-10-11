a3f5a91eba0e6302bee6521966a284e29790f0c0
wincenzo <wincenzo@gmail.com>
2023-10-11 20:06:32 +0200

    update lottery.py

--- lottery.py
+++ lottery.py
@@ -121,7 +121,7 @@ class Lottery:
 
 
 if __name__ == '__main__':
-    print('Starting...')
+    print('Inizio...')
     superenalotto = Lottery(max_numbers=90, max_extra=90,
                             len_numbers=6, len_extra=0)
     superenalotto(backend='choice', many=1_000_000).draw()
