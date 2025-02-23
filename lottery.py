8a047f8468784f315cb941b6a29a1edf8baf8f7d
wincenzo <wincenzo@gmail.com>
2025-02-23 17:18:22 +0100

    fixed

--- lottery.py
+++ lottery.py
@@ -4,7 +4,6 @@ import sys
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
 from datetime import datetime
-from functools import cached_property, lru_cache
 from itertools import islice, repeat, starmap
 from operator import itemgetter
 from random import SystemRandom
@@ -25,10 +24,11 @@ class Lottery:
         'max_num',
         'max_ext',
         'draw_sz',
-        'ext_sz',
+        'xtr_sz',
         '_iters',
         'result',
         '_method',
+        '__dict__',
     )
 
     rnd = SystemRandom()
@@ -37,17 +37,21 @@ class Lottery:
                  max_num: int = MAX_NUMBERS,
                  draw_sz: int = DEFAULT_DRAW_SIZE,
                  max_ext: Optional[int] = None,
-                 ext_sz: Optional[int] = None,
+                 xtr_sz: Optional[int] = None,
                  ) -> None:
 
         self.max_num = max_num or self.MAX_NUMBERS
         self.max_ext = max_ext or 0
         self.draw_sz = draw_sz or self.DEFAULT_DRAW_SIZE
-        self.ext_sz = ext_sz or 0
+        self.xtr_sz = xtr_sz or 0
         self._iters: int = 0
         self.result: Extraction = Extraction((), None)
 
-    @cached_property
+    @property
+    def number_range(self) -> list[int]:
+        return list(range(1, self.max_num+1))
+
+    @property
     def default(self) -> DrawMethod:
         def_bcknd, = self.rnd.sample(self.BACKENDS, k=1)
         return getattr(self, def_bcknd)
@@ -60,7 +64,8 @@ class Lottery:
     def method(self, name: str) -> None:
         if not self.is_valid_backend(name):
             self._method = self.default
-        self._method = cast(DrawMethod, getattr(self, name))
+        else:
+            self._method = cast(DrawMethod, getattr(self, name))
 
     @staticmethod
     def is_valid_backend(name: str) -> TypeGuard[str]:
@@ -72,39 +77,29 @@ class Lottery:
         try:
             yield self
         finally:
-            self._iters = 0
-            self.result = Extraction((), None)
+            pass
 
-    @lru_cache(maxsize=128)
-    def get_number_range(self, max_num: int) -> list[int]:
-        return list(range(1, max_num + 1))
-
-    @validate_draw_params
     def choice(self, size: int, max_num: int) -> tuple[int, ...]:
-        numbers = self.get_number_range(max_num)
+        numbers = self.number_range
 
         def draw():
             nonlocal max_num
-            idx = self.rnd.randrange(max_num)
+            idx = self.rnd.choice(range(max_num))
             number = numbers.pop(idx)
             max_num -= 1
             return number
 
         return tuple(starmap(draw, repeat((), size)))
 
-    @validate_draw_params
     def sample(self, size: int, max_num: int) -> tuple[int, ...]:
         indexes = itemgetter(*self.rnd.sample(range(max_num), k=size))
-        numbers = indexes(self.get_number_range(max_num))
+        numbers = indexes(self.number_range)
 
-        if isinstance(numbers, tuple):
-            return numbers
-        else:
-            return (numbers,)
+        return numbers if isinstance(numbers, tuple) else (numbers,)
 
-    @validate_draw_params
     def randint(self, size: int, max_num: int) -> tuple[int, ...]:
         draw = iter(lambda: self.rnd.randint(1, max_num), None)
+
         extraction = set()
         while True:
             extraction.add(next(draw))
@@ -113,14 +108,13 @@ class Lottery:
 
         return tuple(extraction)
 
-    @validate_draw_params
     def shuffle(self, size: int, max_num: int) -> tuple[int, ...]:
-        numbers = self.get_number_range(max_num)
+        numbers = self.number_range
         self.rnd.shuffle(numbers)
 
         start = Lottery.rnd.randint(0, max_num-size)
         max_numstep = (max_num - start) // size
-        step = Lottery.rnd.randint(1, max_numstep)
+        step = Lottery.rnd.randrange(1, max_numstep)
         stop = start + size * step
         grab = slice(start, stop, step)
 
@@ -129,6 +123,7 @@ class Lottery:
     def draw_once(self, size: int, max_num: int) -> tuple[int, ...]:
         return self.method(size, max_num) if all((size, max_num)) else ()
 
+    @validate_draw_params
     async def drawer(self, size: int, max_num: int) -> tuple[int, ...]:
         """Adds randomness by simulating multiple draws and grabbing last one."""
         loop = asyncio.get_event_loop()
@@ -137,21 +132,23 @@ class Lottery:
             futures = [
                 loop.run_in_executor(executor, self.draw_once, size, max_num)
                 for _ in tqdm(range(self._iters),
-                              desc=f"Backend: {self._method.__name__} ...",
-                              unit="draws")
+                              desc=f"Backend: {self.method.__name__}",
+                              unit="draws",
+                              ncols=80,
+                              bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]')
             ]
-            completed = await asyncio.gather(*futures)
+        completed = await asyncio.gather(*futures)
 
         return next(islice(completed, self._iters-1, None))
 
     def __call__(self, method: str, many: Optional[int] = None) -> Self:
         with self.drawing_session():
             self.method = method
-            self._iters = self.rnd.randint(1, many or 1)
+            self._iters = many or self.rnd.randint(1, 100_000)
 
             draw = asyncio.run(self.drawer(self.draw_sz, self.max_num))
-            extra = asyncio.run(self.drawer(
-                self.ext_sz, self.max_ext)) if self.ext_sz else ()
+            extra = asyncio.run(
+                self.drawer(self.xtr_sz, self.max_ext)) if self.xtr_sz else ()
 
             self.result = Extraction(draw, extra)
 
@@ -159,40 +156,40 @@ class Lottery:
 
     def __str__(self) -> str:
         now = datetime.now()
-        draw = ' '.join(map(str, self.result.draw))
+        draw = ' '.join(map(str, sorted(self.result.draw)))
         result = f'\nEstrazione del {now:%x %X}\nNumeri estratti: {draw}'
 
         if self.result.extra:
-            extra = ' '.join(map(str, self.result.extra))
+            extra = ' '.join(map(str, sorted(self.result.extra)))
             result += f'\nSuperstar: {extra}'
 
         return result
 
     def __repr__(self) -> str:
         return (f'Lottery(max_num={self.max_num}, max_ext={self.max_ext},'
-                f' draw_sz={self.draw_sz}, ext_sz={self.ext_sz})')
+                f' draw_sz={self.draw_sz}, xtr_sz={self.xtr_sz})')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Lottery number generator')
 
-    parser.add_argument('-m', '--many', action='store', default=100_000, type=int,
+    parser.add_argument('-m', '--many', action='store', default=None, type=int,
                         help='select how many times to draw')
     parser.add_argument('-n', '--numbers', action='store', default=90, type=int,
                         help='select upper limit for numbers')
     parser.add_argument('-e', '--extras', action='store', default=90, type=int,
                         help='select upper limit for extras')
-    parser.add_argument('--nsz', action='store', default=6, type=int,
+    parser.add_argument('--numsz', action='store', default=6, type=int,
                         help='select how many numbers to draw')
-    parser.add_argument('--esz', action='store', default=0, type=int,
+    parser.add_argument('--xtrsz', action='store', default=0, type=int,
                         help='select how many extra numbers to draw')
 
     args = parser.parse_args()
 
     try:
         superenalotto = Lottery(
-            max_num=args.numbers, draw_sz=args.nsize,
-            max_ext=args.extras, ext_sz=args.esize
+            max_num=args.numbers, draw_sz=args.numsz,
+            max_ext=args.extras, xtr_sz=args.xtrsz
         )
 
         method = input(
--- utils.py
+++ utils.py
@@ -1,21 +1,23 @@
-from functools import wraps
-from typing import Protocol, Optional
 from dataclasses import dataclass
+from functools import wraps
+from typing import Callable, Optional, Protocol
 
 
-def validate_draw_params(func):
+def validate_draw_params(func) -> Callable:
     @wraps(func)
-    def wrapper(size: int, max_num: int, *args, **kwargs):
+    def wrapper(self, size: int, max_num: int, *args, **kwargs):
         if not 0 < size <= max_num:
             raise ValueError(
                 f"Invalid draw parameters: size={size}, max_num={max_num}")
-        return func(size, max_num, *args, **kwargs)
+        return func(self, size, max_num, *args, **kwargs)
     return wrapper
 
 
 class DrawMethod(Protocol):
     __name__: str
-    def __call__(self, size: int, max_num: int) -> tuple[int]: ...
+
+    def __call__(self, size: int, max_num: int) -> tuple[int]:
+        ...
 
 
 @dataclass(frozen=True, slots=True)
